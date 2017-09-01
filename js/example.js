60d838c9752ac9615b98f20dab83b0ce7168680e
Carlitoshsh <caragomezher@unal.edu.co>
2017-09-01 05:14:29 -0500

    :c

--- js/example.js
+++ js/example.js
@@ -1,3 +1,34 @@
+function ajax(url, method, params, callback) {
+  var obj;
+
+  try {
+    obj = new XMLHttpRequest();
+  } catch (e) {
+    try {
+      obj = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        obj = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e) {
+        alert("Your browser does not support Ajax.");
+        return false;
+      }
+    }
+  }
+  obj.onreadystatechange = function () {
+    if (obj.readyState == 4) {
+      callback(obj);
+    }
+  };
+  obj.open(method, url, true);
+  obj.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+  obj.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  obj.send(params);
+
+  return obj;
+}
+
+
 var
     $ = function (id) {
         return document.getElementById(id);
@@ -66,7 +97,7 @@ function coverRenderer(instance, td, row, col, prop, value, cellProperties) {
 }
 
 Handsontable.dom.addEvent(load, 'click', function () {
-    ajax('json/dataload.json', 'GET', '', function (res) {
+    ajax('json/load.json', 'GET', '', function (res) {
         var data = JSON.parse(res.response);
 
         hot.loadData(data.data);
